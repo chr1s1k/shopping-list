858b65929e68409725017915838da5ea59c39bbd
Radek Sukup <radek.sukup@actum.cz>
2019-08-28 15:56:54 +0200

    Test of shopping list added.

---
+++ src/components/ShoppingList/ShoppingList.test.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { shallow, render } from 'enzyme'
+
+import ShoppingList from './ShoppingList'
+// import ShoppingListItem from './ShoppingListItem'
+
+describe('Shopping list test', () => {
+	it('should render some items', () => {
+		const items = [
+			{
+				value: 'apple',
+			},
+			{
+				value: 'carrot',
+			},
+			{
+				value: 'onion',
+			}
+		]
+		const component = shallow(<ShoppingList items={items} />)
+
+		// console.log(component.debug())
+
+		// expect(component).toMatchSnapshot()
+		expect(component.find('.list-items')).toBeDefined()
+		// expect(component.find('.list-group-item')).toHaveLength(items.length)
+		expect(component.find('ShoppingListItem').length).toBe(items.length)
+	})
+})
